da9271e34bfc61a15bf0eda61b1b0762cca00e6b
Seth Perritt <sethperritt@yahoo.com>
2022-07-21 11:19:44 -0700

    fixed navbar layout

--- src/components/NavbarMain.js
+++ src/components/NavbarMain.js
@@ -2,6 +2,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import React from "react";
+import "../stylesheets/navbar.css";
 
 function NavbarMain() {
   return (
